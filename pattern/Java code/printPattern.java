c6ae624169c1afdaeceeba2206dda0f24c171063
Sohail Shaikh <sohailshaikh062003@gmail.com>
2026-09-02 12:27:29 +0530

    Add new patterns (14-19) to printPattern.java and update main method

--- pattern/Java code/printPattern.java
+++ pattern/Java code/printPattern.java
@@ -177,6 +177,99 @@ public class printPattern {
         }
     }
     
+    // pattern 14
+    public static void pattern14(int n) {
+        for (int i=0; i<n; i++) {
+            for (char ch='A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 15
+    public static void pattern15(int n) {
+        for (int i=0; i<n; i++) {
+            for (char ch='A'; ch <= 'A' + (n-i-1); ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 16
+    public static void pattern16(int n) {
+        for (int i=0; i<n; i++){
+            char ch = (char) ('A' + i);
+            for (int j=0; j<=i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 17
+    public static void pattern17(int n) {
+        for (int i=0; i<n; i++) {
+            // space
+            for (int j=0; j<n-i-1; j++){  
+                System.out.print(" ");
+            }
+
+            // char
+            char ch='A';
+            int breakPoint = (2*i+1)/2;
+            for (int j=0; j < 2*i+1; j++) {   
+                System.out.print(ch);
+                if (j < breakPoint) ch++;
+                else ch--;
+            }
+
+            // space
+            for (int j=0; j<n-i-1; j++){  
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+
+
+    // pattern 18
+    public static void pattern18(int n) {
+        for (int i=0; i<n; i++) {
+            for (char ch= (char)('E' - i); ch <= 'E'; ch++){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+
+    // pattern 19
+    public static void pattern19(int n) {
+        int space = 0;
+        // first part 
+        for (int i=0; i<n; i++) {
+            // start
+            for (int j=1; j<=n-i; j++) {
+                System.out.print("*");
+            }
+
+            // space 
+            for (int j=0; j<space; j++){
+                System.out.print(" ");
+            }
+
+            // start
+            for (int j=1; j<=n-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            space += 2;
+        }
+
+    }
+
     public static void main(String[] args) { 
         
         // squarePattern(5);
@@ -190,6 +283,12 @@ public class printPattern {
         // printSideTriangleStar(5);
         // RightAngleTriangleWithZeroAndOne(5);
         // pattern12(4);
-        pattern13(5);
+        // pattern13(5);
+        // pattern14(5);
+        // pattern15(5);
+        // pattern16(5);
+        // pattern17(5);
+        // pattern18(5);
+        pattern19(5);
     }
 }
